aff9e71024d72bafbe534326c33f94b21e1c2d6a
keisuke-suzuki <keisuke.suzuki@woven-planet.global>
2023-12-11 17:23:49 +0900

    solved 01_challenge

---
+++ 01_introduction_to_cpp/01_challenge/main.cpp
@@ -0,0 +1,13 @@
+// 不足している値、データ型、その他のコードを追加してプログラムを完成させてください。
+#include <iostream>
+
+int main() {
+    int myNumber = 1;
+
+    int anotherNumber = 5;
+    
+    int answer = myNumber + anotherNumber;
+
+    std::cout << answer << std::endl;
+    return 0;
+}
